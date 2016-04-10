ebaa9a7c85514f293c9b4b5fe6149dad5120d4d1
Fabien Toune <github@lapin-blanc.com>
2016-04-10 08:40:35 +0200

    Implémentation de send_email comme thread
    Pour ne pas bloquer le programme appelant lors de l'envoi d'un mail

--- google_mail.py
+++ google_mail.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
+import threading
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
 def send_email(fromaddr, password, toaddr, subject, message):
-    msg = MIMEMultipart('alternative')
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = subject
-    body = message
-    msg.attach(MIMEText(body, 'html'))
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, password)
-    text = msg.as_string()
-    server.sendmail("fromaddr", toaddr, text)
-    server.quit()
+
+    def _send_email(fromaddr, password, toaddr, subject, message):
+        msg = MIMEMultipart('alternative')
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = subject
+        body = message
+        msg.attach(MIMEText(body, 'html'))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, password)
+        text = msg.as_string()
+        server.sendmail("fromaddr", toaddr, text)
+        server.quit()
+    threading.Thread(
+        target=send_email,
+        args=(fromaddr, password, toaddr, subject, message)
+            ).start()
